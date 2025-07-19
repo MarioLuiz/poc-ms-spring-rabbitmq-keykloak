0f709863e79f8771e20e0d341babbb767752473e
Mario Luiz <marioluiz.arruda@gmail.com>
2025-07-19 19:34:51 -0400

    feat: configuracao inicila do eureka server

---
+++ eurekaserver/src/test/kotlin/io/github/poc_ms_spring_rabbitmq_keykloak/eurekaserver/EurekaserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.poc_ms_spring_rabbitmq_keykloak.eurekaserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
