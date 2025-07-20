36e6ed58783bafc146fbf73c775cdf41fbee850c
Mario Luiz <marioluiz.arruda@gmail.com>
2025-07-20 16:47:15 -0400

    feat: initial config msclients

---
+++ msclients/src/main/kotlin/io/gitgub/poc_ms_spring_rabbitmq_keykloak/msclients/MsclientsApplication.kt
@@ -0,0 +1,10 @@
+package io.gitgub.poc_ms_spring_rabbitmq_keykloak.msclients
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsclientsApplication
+fun main(args: Array<String>) {
+	runApplication<MsclientsApplication>(*args)
+}
---
+++ msclients/src/test/kotlin/io/gitgub/poc_ms_spring_rabbitmq_keykloak/msclients/MsclientsApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.gitgub.poc_ms_spring_rabbitmq_keykloak.msclients
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsclientsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
