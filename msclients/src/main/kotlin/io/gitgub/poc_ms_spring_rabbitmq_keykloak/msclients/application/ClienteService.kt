af13dc360762e5560ead591fbfafafce74b840fc
Mario Luiz <marioluiz.arruda@gmail.com>
2025-07-23 21:48:06 -0400

    feat: add cliente service

---
+++ msclients/src/main/kotlin/io/gitgub/poc_ms_spring_rabbitmq_keykloak/msclients/application/ClienteService.kt
@@ -0,0 +1,18 @@
+package io.gitgub.poc_ms_spring_rabbitmq_keykloak.msclients.application
+
+import io.gitgub.poc_ms_spring_rabbitmq_keykloak.msclients.domain.Cliente
+import io.gitgub.poc_ms_spring_rabbitmq_keykloak.msclients.infra.repository.ClienteRepository
+import jakarta.transaction.Transactional
+
+class ClienteService(
+    private val repository: ClienteRepository
+) {
+    @Transactional
+    fun save(cliente: Cliente): Cliente {
+        return repository.save(cliente)
+    }
+
+    fun getByCpf(cpf: String) : Cliente?{
+        return repository.findByCpf(cpf)
+    }
+}
