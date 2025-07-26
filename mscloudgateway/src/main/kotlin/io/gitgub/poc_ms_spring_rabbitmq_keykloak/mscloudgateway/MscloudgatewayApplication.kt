9d950117480ca7bd8893acc55519909b2b87add6
Mario Luiz <marioluiz.arruda@gmail.com>
2025-07-26 16:43:38 -0400

    feat: initial config gateway

---
+++ mscloudgateway/src/main/kotlin/io/gitgub/poc_ms_spring_rabbitmq_keykloak/mscloudgateway/MscloudgatewayApplication.kt
@@ -0,0 +1,24 @@
+package io.gitgub.poc_ms_spring_rabbitmq_keykloak.mscloudgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class MscloudgatewayApplication {
+	@Bean
+	fun routes(builder: RouteLocatorBuilder): RouteLocator{
+		return builder
+			.routes()
+			//.route(r -> r.path("/clientes/**").uri("lb://msclients"))
+			.route { r -> r.path("/clientes/**").uri("lb://msclients") }
+			.build()
+	}
+}
+fun main(args: Array<String>) {
+	runApplication<MscloudgatewayApplication>(*args)
+}
---
+++ mscloudgateway/src/test/kotlin/io/gitgub/poc_ms_spring_rabbitmq_keykloak/mscloudgateway/MscloudgatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.gitgub.poc_ms_spring_rabbitmq_keykloak.mscloudgateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
