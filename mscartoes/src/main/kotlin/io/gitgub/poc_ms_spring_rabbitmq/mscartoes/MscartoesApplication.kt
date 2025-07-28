6829d6675593bce28a6aff15b6ba4641ff44559b
MarioLuiz <marioluiz.arruda@gmail.com>
2025-07-27 23:03:59 -0400

    feat: initial structure mscartoes

---
+++ mscartoes/src/main/kotlin/io/gitgub/poc_ms_spring_rabbitmq/mscartoes/MscartoesApplication.kt
@@ -0,0 +1,11 @@
+package io.gitgub.poc_ms_spring_rabbitmq.mscartoes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MscartoesApplication
+
+fun main(args: Array<String>) {
+	runApplication<MscartoesApplication>(*args)
+}
---
+++ mscartoes/src/test/kotlin/io/gitgub/poc_ms_spring_rabbitmq/mscartoes/MscartoesApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.gitgub.poc_ms_spring_rabbitmq.mscartoes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MscartoesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
