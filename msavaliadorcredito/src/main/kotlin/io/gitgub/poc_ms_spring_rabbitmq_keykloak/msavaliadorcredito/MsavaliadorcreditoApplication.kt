b5d876695f832dd7a9d3f04da558029b58209d93
Mario Luiz <marioluiz.arruda@gmail.com>
2025-10-04 23:14:31 -0400

    feat: estrutura inicial ms avaliador credito

---
+++ msavaliadorcredito/src/main/kotlin/io/gitgub/poc_ms_spring_rabbitmq_keykloak/msavaliadorcredito/MsavaliadorcreditoApplication.kt
@@ -0,0 +1,11 @@
+package io.gitgub.poc_ms_spring_rabbitmq_keykloak.msavaliadorcredito
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsavaliadorcreditoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsavaliadorcreditoApplication>(*args)
+}
---
+++ msavaliadorcredito/src/test/kotlin/io/gitgub/poc_ms_spring_rabbitmq_keykloak/msavaliadorcredito/MsavaliadorcreditoApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.gitgub.poc_ms_spring_rabbitmq_keykloak.msavaliadorcredito
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsavaliadorcreditoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
