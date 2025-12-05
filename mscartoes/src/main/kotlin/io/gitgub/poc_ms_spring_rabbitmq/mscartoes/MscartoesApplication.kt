4c49268bb1320f53f1c925a8d06291e7ded647d2
Mario Luiz <marioluiz.arruda@gmail.com>
2025-12-05 00:48:20 -0400

    feat: config inicial rabbitmq.

--- mscartoes/src/main/kotlin/io/gitgub/poc_ms_spring_rabbitmq/mscartoes/MscartoesApplication.kt
+++ mscartoes/src/main/kotlin/io/gitgub/poc_ms_spring_rabbitmq/mscartoes/MscartoesApplication.kt
@@ -1,9 +1,11 @@
 package io.gitgub.poc_ms_spring_rabbitmq.mscartoes
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableRabbit
 class MscartoesApplication
 fun main(args: Array<String>) {
 	runApplication<MscartoesApplication>(*args)
---
+++ mscartoes/src/main/kotlin/io/gitgub/poc_ms_spring_rabbitmq/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.kt
@@ -0,0 +1,18 @@
+package io.gitgub.poc_ms_spring_rabbitmq.mscartoes.infra.mqueue
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.stereotype.Component
+
+@Component
+class EmissaoCartaoSubscriber {
+
+    @Value("\${mq.queues.emissao - cartoes}")
+    private lateinit var fila : String
+
+    @RabbitListener(queues = ["\${mq.queues.emissao - cartoes}"])
+    fun receberSolicitacaoEmissao(@Payload payload: String) {
+        println(payload)
+    }
+}
